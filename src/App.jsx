a5b989f34c4123d78e68aabf168ccb84ad9c54ab
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2023-01-27 08:21:45 +0530

    created the base structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-9xl font-bold underline text-white">Hello world!</h1>
+  );
+}
+
+export default App;
---
+++ src/Components/heading.jsx
@@ -0,0 +1,5 @@
+import "./heading.css";
+
+function heading() {
+  return <h1 className="text-cyan-600 text-6xl"> Counter App</h1>;
+}
